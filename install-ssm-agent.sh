93b1fea576408b5c66e1b8ade23299ed4d0d3786
hieutt1991 <hieu2t.system@gmail.com>
2023-03-10 12:15:42 +0700

    Update install-ssm-agent.sh

--- install-ssm-agent.sh
+++ install-ssm-agent.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-# Install SSM Agent on RHEL
-# Download SSM Agent package
 sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-# Enable SSM Agent service
 sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
